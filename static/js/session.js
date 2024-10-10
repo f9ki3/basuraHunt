c9abd54f711b9581546286893eb69848e80d15da
Fyke Lleva <floterina@gmail.com>
2024-10-11 02:11:30 +0800

    updated

--- static/js/session.js
+++ static/js/session.js
@@ -8,8 +8,9 @@ function session() {
             data = JSON.parse(data)
             // console.log(data)
             $('#fname_home').text("Hello " + data.fname)
+            $('#fullname_profile').text(data.fname + " " + data.lname)
+            $('#profileImage').attr('src', 'static/uploads/' + data.profile);
 
-            
         },
         error: function (xhr, status, error) {
             console.error('Error fetching reports:', error);
@@ -17,4 +18,5 @@ function session() {
     });
     
 }
+
 session()
