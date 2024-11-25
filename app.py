cbcbbd2804fbbbfa399dbe7bf7b6993464c8212a
Fyke Lleva <floterina@gmail.com>
2024-11-25 08:20:11 +0800

    updated

--- app.py
+++ app.py
@@ -677,7 +677,7 @@ def process_trash2():
     dispose_value = data.get('dispose')  # Extract the 'dispose' value
     
     # Get the current date and time
-    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Format as a string
+    current_date = 'datetime.now().strftime("%Y-%m-%d %H:%M:%S")'  # Format as a string
 
     type = 'Trash Bin 2'
 
